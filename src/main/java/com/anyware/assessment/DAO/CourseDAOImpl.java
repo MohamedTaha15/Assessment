531e04533398164b1e03c58d6011b932e8168a7a
MohamedTaha <mohamedtaha.9718@gmail.com>
2024-02-17 02:20:20 +0200

    code structure and service beans update

--- src/main/java/com/anyware/assessment/DAO/CourseDAOImpl.java
+++ src/main/java/com/anyware/assessment/DAO/CourseDAOImpl.java
@@ -3,13 +3,16 @@ package com.anyware.assessment.DAO;
 import com.anyware.assessment.model.Course;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
 
 public class CourseDAOImpl implements CourseDAO{
 
     private SessionFactory sessionFactory;
-
     public CourseDAOImpl(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
